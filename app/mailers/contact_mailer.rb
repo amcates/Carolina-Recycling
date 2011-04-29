8dd6c78d34a4edbfd07ee4e02216ba4c72e8d724
Andrew Cates <catesa@musc.edu>
2011-04-29 10:25:35 -0400

    making e-mail come from requester

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,6 @@
 class ContactMailer < ActionMailer::Base
-  default :from => "contact@carolinarecyclingcompany.com"
-
   def send_contact(contact)
     @contact = contact
-    mail(:to => ['caleb@carolinarecyclingcompany.com', 'Natalie@carolinarecyclingcompany.com', 'info@carolinarecyclingcompany.com'], :subject => "Website Contact Request")
+    mail(:from => @contact.email, :to => ['caleb@carolinarecyclingcompany.com', 'Natalie@carolinarecyclingcompany.com', 'info@carolinarecyclingcompany.com'], :subject => "Website Contact Request")
   end
 end
