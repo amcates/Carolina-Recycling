a8437eefe773e99e62d1de3c3a54d8344f6c959f
Andrew Cates <amcates@gmail.com>
2012-08-23 17:01:22 -0400

    fixes and updates to site

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ActionMailer::Base
   def send_contact(contact)
     @contact = contact
-    mail(:from => @contact.email, :to => ['caleb@carolinarecyclingcompany.com', 'Natalie@carolinarecyclingcompany.com', 'info@carolinarecyclingcompany.com'], :subject => "Website Contact Request")
+    mail(:from => @contact.email, :to => ['caleb@carolinarecyclingcompany.com', 'natalie@carolinarecyclingcompany.com', 'info@carolinarecyclingcompany.com'], :subject => "Website Contact Request")
   end
 end
